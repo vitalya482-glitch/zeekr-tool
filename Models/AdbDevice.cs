6b3e5a40c7fe3e0f3592b6c61ffb3e18f5b91dec
vitalya482-glitch <vitalya482@gmail.com>
2026-04-30 13:33:08 +0500

    Update AdbDevice.cs

--- Models/AdbDevice.cs
+++ Models/AdbDevice.cs
@@ -1,1 +1,20 @@
+namespace ZeekrTool.Models
+{
+    // ZEEKR_TOOL_MARKER: MODEL_ADB_DEVICE
+    public class AdbDevice
+    {
+        public string Id { get; set; } = "";
+        public string State { get; set; } = "";
+        public string ConnectionType { get; set; } = "";
+        public string Model { get; set; } = "";
 
+        public string DisplayName
+        {
+            get
+            {
+                string modelPart = string.IsNullOrWhiteSpace(Model) ? "" : $" ({Model})";
+                return $"{Id} | {State} | {ConnectionType}{modelPart}";
+            }
+        }
+    }
+}
