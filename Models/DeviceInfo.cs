17157294ddd09fb2bc7425a57476a3834d53778e
vitalya482-glitch <vitalya482@gmail.com>
2026-04-30 13:33:31 +0500

    Update DeviceInfo.cs

--- Models/DeviceInfo.cs
+++ Models/DeviceInfo.cs
@@ -1,1 +1,25 @@
+namespace ZeekrTool.Models
+{
+    // ZEEKR_TOOL_MARKER: MODEL_DEVICE_INFO
+    public class DeviceInfo
+    {
+        public string Id { get; set; } = "";
+        public string ConnectionType { get; set; } = "";
 
+        public string Model { get; set; } = "";
+        public string Brand { get; set; } = "";
+        public string Manufacturer { get; set; } = "";
+        public string Device { get; set; } = "";
+        public string Hardware { get; set; } = "";
+
+        public string AndroidVersion { get; set; } = "";
+        public string SdkVersion { get; set; } = "";
+        public string BuildId { get; set; } = "";
+
+        public string CpuAbi { get; set; } = "";
+        public string CpuAbiList { get; set; } = "";
+
+        public string ScreenSize { get; set; } = "";
+        public string Density { get; set; } = "";
+    }
+}
