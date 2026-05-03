061f0a137816dcf90a61e38f343b7921aa7272aa
vitalya482-glitch <vitalya482@gmail.com>
2026-05-03 20:51:34 +0500

    Update AppInfo.cs

--- Models/AppInfo.cs
+++ Models/AppInfo.cs
@@ -1,1 +1,10 @@
-
+namespace ZeekrTool.Models
+{
+    // ZEEKR_TOOL_MARKER: MODEL_APP_INFO
+    public class AppInfo
+    {
+        public string PackageName { get; set; } = "";
+        public string Type { get; set; } = "User";
+        public string ApkPath { get; set; } = "";
+    }
+}
